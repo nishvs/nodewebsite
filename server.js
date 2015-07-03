8dc3d03ecf792687b8fd7d55800dcabe09bd4542
Nitish <nitishsv1990@gmail.com>
2015-07-03 15:38:15 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express')
+, path = require('path');
+var app = express();
+
+
+// Add headers
+// app.use(function (req, res, next) {
+//
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//
+//     // Pass to next layer of middleware
+//     next();
+// });
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/j', function(req, res) {
+    res.render('dtable');
+});
+
+app.get('/h', function(req, res) {
+    res.render('hindex');
+});
+
+var server = app.listen(3000, function() {
+    console.log("Website on port 3000");
+});
