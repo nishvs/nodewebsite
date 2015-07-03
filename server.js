c65031813619cb332091e02663fd29af3cb7bb7b
Nitish <nitishsv1990@gmail.com>
2015-07-03 15:54:47 +0530

    2nd Commit

--- server.js
+++ server.js
@@ -40,6 +40,12 @@ app.get('/h', function(req, res) {
     res.render('hindex');
 });
 
-var server = app.listen(3000, function() {
-    console.log("Website on port 3000");
+// var server = app.listen(3000, function() {
+//     console.log("Website on port 3000");
+// });
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
